43f3bc459b76c51e6f6c2c4089ee551fc6169f95
jaskier07 <aleksanderkania96@gmail.com>
2019-12-28 14:58:03 +0100

    Code refactor

--- mergesort-cuda.cu
+++ mergesort-cuda.cu
@@ -7,31 +7,34 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-#define HARD_DBG 0
+#define PROGRAM_STATE 2
+
+#define HARD_DBG 2
 #define DBG 1
-#define SHOWTIME 0
 
-#define NUMBERS_BIG 4194304*2//4 194 304 //100 000 000 //2000000
-#define NUMBERS_DBG 4194304 //128
+#define VECTOR_SIZE_STD 4194304*2 ///100 000 000
+#define VECTOR_SIZE_DBG 128 // 128
 
-#define MAX_BIG 100000 //1000000
-#define MAX_DBG 999
+#define VECTOR_MAX_NUMBER_STD 999 //1000000
+#define VECTOR_MAX_NUMBER_DBG 100 // 100
 
-#define NUMBERS ((DBG == 1) ? NUMBERS_DBG : NUMBERS_BIG)
-#define MAX_NUMBER ((DBG == 1) ? MAX_DBG : MAX_BIG)
-#define THREADS_PER_BLOCK 512
-#define VECTOR_LENGTH_PER_THREAD 2
-#define VECTOR_MULTIPLIER 2
+#define THREADS_PER_BLOCK_STD 512
+#define THREADS_PER_BLOCK_DBG 32 // 32
+
+#define VECTOR_LENGTH_PER_THREAD_STD 32
+#define VECTOR_LENGTH_PER_THREAD_DBG 2 //2
+
+#define VECTOR_SIZE ((PROGRAM_STATE >= DBG) ? VECTOR_SIZE_DBG : VECTOR_SIZE_STD)
+#define MAX_NUMBER ((PROGRAM_STATE >= DBG) ? VECTOR_MAX_NUMBER_DBG : VECTOR_MAX_NUMBER_STD)
+#define THREADS_PER_BLOCK ((PROGRAM_STATE >= DBG) ? THREADS_PER_BLOCK_DBG : THREADS_PER_BLOCK_STD)
+#define VECTOR_LENGTH_PER_THREAD ((PROGRAM_STATE >= DBG) ? VECTOR_LENGTH_PER_THREAD_DBG : VECTOR_LENGTH_PER_THREAD_STD)
 
 /* cuda errors */
-bool checkForError(const cudaError_t cudaStatus, const char text[], short* dev_input, short* dev_tmp=NULL) {
+bool checkForError(const cudaError_t cudaStatus, const char text[], short* dev_input) {
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "\n\n%s \nError code: %d \nStatus: %s \n\n", text, cudaStatus, cudaGetErrorString(cudaStatus));
 		if (dev_input != NULL) {
 			cudaFree(dev_input);
-		}
-		if (dev_tmp != NULL) {
-			cudaFree(dev_tmp);
 		}
 		return true;
 	}
@@ -57,7 +60,7 @@ void printArray(short* A, int size) {
 }
 
 void checkIfCorrectlySorted(short* arr) {
-	for (int i = 0; i < NUMBERS - 1; i++) {
+	for (int i = 0; i < VECTOR_SIZE - 1; i++) {
 		if (arr[i] > arr[i + 1]) {
 			printf("\n\n-----------ERROR!-----------%d\n\n ", i);
 			return;
@@ -70,12 +73,12 @@ void checkIfCorrectlySorted(short* arr) {
 void fillArrayWithNumbers(short* numbers) {
 	int i;
 	srand(time(NULL));
-	for (i = 0; i < NUMBERS; i++) {
+	for (i = 0; i < VECTOR_SIZE; i++) {
 		numbers[i] = rand() % MAX_NUMBER;
 	}
 
-	if (HARD_DBG) {
-		printArray(numbers, NUMBERS);
+	if (PROGRAM_STATE >= HARD_DBG) {
+		printArray(numbers, VECTOR_SIZE);
 	}
 }
 
@@ -139,7 +142,7 @@ void mergeKernel(short* arr, int vectorLengthPerThread, int vectorLength, int tm
 	int mid = getMid(leftStart, rightEnd);
 	
 	if (leftStart < vectorLength) {
-		if (HARD_DBG) {
+		if (PROGRAM_STATE >= HARD_DBG) {
 			printf("\n thread: %d, <%d, %d>, mid %d", threadId, leftStart, rightEnd, mid);
 		}
 		merge(arr, leftStart, rightEnd, mid, tmpIndexStart);
@@ -148,8 +151,8 @@ void mergeKernel(short* arr, int vectorLengthPerThread, int vectorLength, int tm
 
 
 void mergeSort(short* arr,int leftStart, int rightEnd, int minVectorLength, int tmpIndexStart) {
-	if (leftStart < rightEnd && rightEnd - leftStart > minVectorLength) {
-		if (HARD_DBG) {
+	if (leftStart < rightEnd && rightEnd - leftStart >= minVectorLength) {
+		if (PROGRAM_STATE >= HARD_DBG) {
 			printf("\n<%d,%d> minVec: %d", leftStart, rightEnd, minVectorLength);
 		}
 		int m = getMid(leftStart, rightEnd);
@@ -160,15 +163,18 @@ void mergeSort(short* arr,int leftStart, int rightEnd, int minVectorLength, int 
 }
 
 int main() {
-	const int vectorSizeInBytes = NUMBERS * sizeof(short) * 2;
-	int tmpIndexStart = NUMBERS;
-	//short* tmp = (short*)malloc(vectorSizeInBytes);
+	const int vectorMultiplier = 2;
+	const int vectorLength = VECTOR_SIZE;
+	int threadsPerBlock = THREADS_PER_BLOCK;
+	int vectorLengthPerThread = VECTOR_LENGTH_PER_THREAD;
+	int numBlocks = ceil(vectorLength / threadsPerBlock);
+	const int blockVectorLength = vectorLength / numBlocks;	
+	const int vectorSizeInBytes = vectorLength * sizeof(short) * 2;
+	int tmpIndexStart = vectorLength;
+	
 	short* vector = (short*)malloc(vectorSizeInBytes);
-
 	fillArrayWithNumbers(vector);
-
 	short* dev_input = NULL;
-	//short* dev_tmp = NULL;
 	cudaError_t cudaStatus;
 
 	cudaStatus = cudaSetDevice(0);
@@ -184,21 +190,14 @@ int main() {
 	cudaStatus = cudaMemcpy(dev_input, vector, vectorSizeInBytes, cudaMemcpyHostToDevice);
 	if (checkForError(cudaStatus, "cudaMemcpy (vector -> dev_input) failed!", dev_input)) {
 		return cudaStatus;
-	}
-	
-	const int vectorLength = NUMBERS;
-	int threadsPerBlock = THREADS_PER_BLOCK;
-	int vectorLengthPerThread = VECTOR_LENGTH_PER_THREAD;
-	const int vectorMultiplier = VECTOR_MULTIPLIER;
-	int numBlocks = ceil(vectorLength / threadsPerBlock);
-	const int blockVectorLength = vectorLength / numBlocks;
+	}	
 	
 	printf("\nConfiguration: vector length: %d, threads per block: %d, vector length per thread: %d, num blocks: %d, block vector length: %d\n",
-		NUMBERS, threadsPerBlock, vectorLengthPerThread, numBlocks, blockVectorLength);
+		vectorLength, threadsPerBlock, vectorLengthPerThread, numBlocks, blockVectorLength);
 
 	int i = 0;
 	while (vectorLengthPerThread <= blockVectorLength) {		
-		if (DBG) {
+		if (PROGRAM_STATE >= DBG) {
 			printf("\nIter: %d, vector length per thread: %d", i++, vectorLengthPerThread);
 		}
 		
@@ -215,7 +214,7 @@ int main() {
 		
 		vectorLengthPerThread *= vectorMultiplier;
 		
-		if (HARD_DBG) {
+		if (PROGRAM_STATE >= HARD_DBG) {
 			cudaStatus = cudaMemcpy(vector, dev_input, vectorSizeInBytes, cudaMemcpyDeviceToHost);
 			if (checkForError(cudaStatus, "cudaMemcpy (dev_input -> vector) failed!")) {
 				return cudaStatus;
@@ -224,15 +223,15 @@ int main() {
 		}
 	}	
 
-	if (!HARD_DBG) {
+	if (PROGRAM_STATE < HARD_DBG) {
 		cudaStatus = cudaMemcpy(vector, dev_input, vectorSizeInBytes, cudaMemcpyDeviceToHost);
 		if (checkForError(cudaStatus, "cudaMemcpy (dev_input -> vector) failed!")) {
 			return cudaStatus;
 		}
 	}
 	
-	mergeSort(vector, 0, NUMBERS - 1, blockVectorLength, tmpIndexStart);
-	if (HARD_DBG) {
+	mergeSort(vector, 0, vectorLength - 1, blockVectorLength, tmpIndexStart);
+	if (PROGRAM_STATE >= HARD_DBG) {
 		printArray(vector, vectorLength);
 	}
 
